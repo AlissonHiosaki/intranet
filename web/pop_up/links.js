ccf17bc6bf430624fbfd0ec7f422b811426cb084
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2024-11-26 21:55:06 -0400

    Add files via upload

--- web/pop_up/links.js
+++ web/pop_up/links.js
@@ -1,9 +1,9 @@
-function openPopup() {
-    document.getElementById('popup').style.display = 'block';
+function openLink() {
+    document.getElementById('link').style.display = 'block';
     document.getElementById('overlay').style.display = 'block';
 }
 
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
+function closeLink() {
+    document.getElementById('link').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
 }
