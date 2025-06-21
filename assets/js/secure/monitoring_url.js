44a52757434a18295eb8d0779ee52f25026d6213
Alisson Hiosaki <92666141+AlissonHiosaki@users.noreply.github.com>
2025-06-20 21:17:59 -0300

    Delete assets directory

--- assets/js/secure/monitoring_url.js
+++
@@ -1,8 +0,0 @@
-// Prevent URL manipulation 
-
-if (window.history && window.history.pushState) {
-    window.history.pushState('', null, './');
-    window.addEventListener('popstate', function() {
-        window.history.pushState('', null, './');
-    });
-}
